41292725bfb72b2f0342bf4c09ce0072d0f0178f
Iunius118 <arcd.01234567@gmail.com>
2018-04-13 05:54:09 +0900

    Fix destroyer's classification

--- src/main/java/iunius118/mods/sunkennavalships/world/gen/WorldGenSunkenDestroyerA.java
+++ src/main/java/iunius118/mods/sunkennavalships/world/gen/WorldGenSunkenDestroyerA.java
@@ -40,7 +40,7 @@ public class WorldGenSunkenDestroyerA extends WorldGenSunkenNavalShip
         int dx = facing.getDirectionVec().getX() * 16;
         int dz = facing.getDirectionVec().getZ() * 16;
 
-        SunkenNavalShips.logger.info("Generate SunkenDestroyer type A between ({}, {}) - ({}, {})", (x1 + dx * 3), (z1 + dz * 3), (x1 + dx * -4), (z1 + dz * -4));
+        SunkenNavalShips.logger.info("Generate SunkenDestroyer A class between ({}, {}) - ({}, {})", (x1 + dx * 3), (z1 + dz * 3), (x1 + dx * -4), (z1 + dz * -4));
 
         // Calculate each section height values
         int depth = random.nextInt(3);
